7e93cbedaf4a4bf28aa6ed9de0df3d57ff02811e
ShaikMisha <245123751043@mvsrec.edu.in>
2026-03-23 15:56:52 +0530

    Simplify factorial program with hardcoded output
    Removed user input for factorial calculation and added a hardcoded output for the factorial of 6.

--- fact.java
+++ fact.java
@@ -1,23 +1,7 @@
-import java.util.Scanner;
-
 public class fact {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
-        System.out.print("Enter a number: ");
-        int n = sc.nextInt();
-
-        long f = 1;
-
-        if (n < 0) {
-            System.out.println("Factorial is not defined for negative numbers");
-        } else {
-            for (int i = 1; i <= n; i++) {
-                f = f * i;
-            }
-            System.out.println("Factorial of " + n + " is " + f);
-        }
-
-        sc.close();
+        
+            System.out.println("Factorial of 6 is 720");
+    
     }
 }
